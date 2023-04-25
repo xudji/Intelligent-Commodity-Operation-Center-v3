3b106d923074533c7605d224fd754b9a567739d3
Ji Xudong <jixudong99@163.com>
2023-04-26 00:16:08 +0800

    feat: Edit data redisplay

--- src/api/product/spu.ts
+++ src/api/product/spu.ts
@@ -1,13 +1,17 @@
 import request from '@/utils/request'
 
+
+
 enum Api {
   getSpuList = '/admin/product',
   getBaseSaleAttrList = '/admin/product/baseSaleAttrList',
   getTrademarkList = '/admin/product/baseTrademark/getTrademarkList',
-  getSaveSpuInfo = '/admin/product/saveSpuInfo'
+  getSaveSpuInfo = '/admin/product/saveSpuInfo',
+  getDelSpu = '/admin/product/deleteSpu'
 }
+
 // 1.获取spu列表
-const reqSpuList = (page: number, limit:number,category3Id:number) => {
+const reqSpuList = (page: number, limit: number, category3Id: number) => {
   return request.get<any, any>(Api.getSpuList + `/${page}/${limit}?category3Id=${category3Id}`)
 }
 
@@ -17,20 +21,25 @@ const reqTrademarkList = () => {
 }
 
 // 3.获取销售属性
-
 const reqBaseSaleAttrList = () => {
   return request.get<any, any>(Api.getBaseSaleAttrList)
 }
-  
+
 //4.保存
 const reqGetSaveSpuInfo = (spuInfo) => {
-  return request.post<any,any>(Api.getSaveSpuInfo , spuInfo )
+  return request.post<any, any>(Api.getSaveSpuInfo, spuInfo)
+}
+
+//5.删除Spu
+const reqDelSpu = (spuId:number) => {
+  return request.delete<any, any>(Api.getDelSpu + `/${spuId} `)
 }
 
 export {
   reqSpuList,
   reqBaseSaleAttrList,
   reqTrademarkList,
-  reqGetSaveSpuInfo
+  reqGetSaveSpuInfo,
+  reqDelSpu
 }
 
--- src/views/product/spu/components/SpuList.vue
+++ src/views/product/spu/components/SpuList.vue
@@ -1,54 +1,92 @@
 <template>
-  <el-card style="margin-top: 10px;">
+  <el-card style="margin-top: 10px">
     <div>
-      <el-button type="primary" :icon="Plus" :disabled="!cateStore.category3Id" @click="AddSpu">添加SPU</el-button>
+      <el-button
+        type="primary"
+        :icon="Plus"
+        :disabled="!cateStore.category3Id"
+        @click="AddSpu"
+        >添加SPU</el-button
+      >
 
-      <el-table border style="margin-top: 10px;" :data="spuList">
-        <el-table-column type="index" label="序号" width="120px"></el-table-column>
+      <el-table border style="margin-top: 10px" :data="spuList">
+        <el-table-column
+          type="index"
+          label="序号"
+          width="120px"
+        ></el-table-column>
         <el-table-column prop="spuName" label="SPU名称" width="260px">
         </el-table-column>
-        <el-table-column prop="description" label="SPU描述">
-        </el-table-column>
+        <el-table-column prop="description" label="SPU描述"> </el-table-column>
         <el-table-column label="操作">
-          <el-tooltip effect="dark" content="添加SPU" placement="top">
-            <el-button type="primary" size="small" :icon="Plus"></el-button>
-          </el-tooltip>
-          <el-tooltip effect="dark" content="修改SPU" placement="top">
-            <el-button type="primary" size="small" :icon="Edit"></el-button>
-          </el-tooltip>
-          <el-tooltip effect="dark" content="查看SPU列表" placement="top">
-            <el-button type="info" size="small" :icon="InfoFilled"></el-button>
-          </el-tooltip>
-          <el-tooltip effect="dark" content="删除SPU" placement="top">
-            <el-button type="danger" size="small" :icon="Delete" @click=""></el-button>
-          </el-tooltip>
+          <template v-slot="{ row }">
+            <el-tooltip effect="dark" content="添加SPU" placement="top">
+              <el-button type="primary" size="small" :icon="Plus"></el-button>
+            </el-tooltip>
+            <el-tooltip effect="dark" content="修改SPU" placement="top">
+              <el-button type="primary" size="small" :icon="Edit"></el-button>
+            </el-tooltip>
+            <el-tooltip effect="dark" content="查看SPU列表" placement="top">
+              <el-button
+                type="info"
+                size="small"
+                :icon="InfoFilled"
+              ></el-button>
+            </el-tooltip>
+            <el-popconfirm :title="`确定删除数据吗?`" @confirm="delSpu(row.id)" >
+              <template #reference>
+                <div style="display: inline-block; margin-left: 10px;">
+                  <el-tooltip
+                    class="box-item"
+                    effect="dark"
+                    content="删除SPU"
+                    placement="top"
+                  >
+                    <el-button
+                      type="danger"
+                      size="small"
+                      :icon="Delete"
+                    ></el-button>
+                  </el-tooltip>
+                </div>
+              </template>
+            </el-popconfirm>
+          </template>
         </el-table-column>
       </el-table>
 
-      <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[3, 5, 7, 11]"
-        :small="small" :disabled="disabled" :background="background" layout="prev, pager, next, jumper, ->, sizes, total"
-        :total="total" @size-change="handleSizeChange" @current-change="handleCurrentChange" />
-
-
+      <el-pagination
+        v-model:current-page="currentPage"
+        v-model:page-size="pageSize"
+        :page-sizes="[3, 5, 7, 11]"
+        :small="small"
+        :disabled="disabled"
+        :background="background"
+        layout="prev, pager, next, jumper, ->, sizes, total"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
     </div>
   </el-card>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 export default defineComponent({
-  name: 'SpuList',
-})
+  name: "SpuList",
+});
 </script>
 <script lang="ts" setup>
-import { ref, onMounted, reactive, watch, nextTick } from 'vue'
+import { ref, onMounted, reactive, watch, nextTick } from "vue";
 import { Plus, Delete, Edit, InfoFilled } from "@element-plus/icons-vue";
-import { reqSpuList } from '@/api/product/spu'
-import { useCategoryListStore } from '@/stores/categoryList'
+import { reqSpuList, reqDelSpu } from "@/api/product/spu";
+import { useCategoryListStore } from "@/stores/categoryList";
+import { ElMessage } from "element-plus";
 
-const cateStore = useCategoryListStore()
-const spuList = ref([])
-const emits = defineEmits(['changeState'])
+const cateStore = useCategoryListStore();
+const spuList = ref([]);
+const emits = defineEmits(["changeState"]);
 // 分页器数据
 const pageSize = ref(3);
 const small = ref(false);
@@ -68,27 +106,36 @@ const handleCurrentChange = (val: number) => {
 
 // 1.获取spu分页数据(需要页码和页数和3id)
 const getSpuList = async () => {
-  const res = await reqSpuList(currentPage.value, pageSize.value, cateStore.category3Id as number)
-  spuList.value = res.records
-}
+  const res = await reqSpuList(
+    currentPage.value,
+    pageSize.value,
+    cateStore.category3Id as number
+  );
+  spuList.value = res.records;
+};
 
 // 2.监听3id变化发请求
 watch(
   () => cateStore.category3Id,
   async (category3Id) => {
-    if (!category3Id) return spuList.value = [];
+    if (!category3Id) return (spuList.value = []);
     // 发请求获取spu列表
-    getSpuList()
+    getSpuList();
   },
   {
-    immediate: true
+    immediate: true,
   }
-)
+);
 
 // 3.添加ASpu
 const AddSpu = () => {
-  emits('changeState', 2)
-}
-
+  emits("changeState", 2);
+};
+// 4.删除SPu
+const delSpu = async (id: number) => {
+  await reqDelSpu(id);
+  ElMessage.success("删除成功");
+  getSpuList()
+};
 </script>
 <style scoped></style>
