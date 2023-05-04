01ad8cdcac65f210b738aac7b710a5a1daf92cd1
Ji Xudong <jixudong99@163.com>
2023-05-04 20:34:31 +0800

    Set up the front page sales chart header

---
+++ src/views/home/monitor/components/SaleForm.vue
@@ -0,0 +1,100 @@
+<template>
+  <el-card class="sale" style="margin-top: 20px">
+    <template #header>
+      <el-menu class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">销售额</el-menu-item>
+        <el-menu-item index="2">访问量</el-menu-item>
+      </el-menu>
+      <!-- 头部右侧 -->
+      <div class="right">
+        <el-radio-group label="size control">
+          <el-radio-button label="今日">今日</el-radio-button>
+          <el-radio-button label="本周">本周</el-radio-button>
+          <el-radio-button label="本月">本月</el-radio-button>
+          <el-radio-button label="本年">本年</el-radio-button>
+        </el-radio-group>
+        <el-date-picker
+          type="daterange"
+          unlink-panels
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          :shortcuts="shortcuts"
+        />
+      </div>
+    </template>
+
+    <div class="sale-content" v-for="o in 4" :key="o">
+      {{ "List item " + o }}
+    </div>
+  </el-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Home",
+});
+</script>
+<script lang="ts" setup>
+import { ref, reactive } from "vue";
+const activeIndex = ref("1");
+const tabPosition = ref("top");
+
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath);
+};
+
+const size = ref<"default" | "large" | "small">("default");
+const value2 = ref("");
+const shortcuts = [
+  {
+    text: "最近一周",
+    value: () => {
+      const end = new Date();
+      const start = new Date();
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+      return [start, end];
+    },
+  },
+  {
+    text: "最近一个月",
+    value: () => {
+      const end = new Date();
+      const start = new Date();
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+      return [start, end];
+    },
+  },
+  {
+    text: "最近三个月",
+    value: () => {
+      const end = new Date();
+      const start = new Date();
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+      return [start, end];
+    },
+  },
+];
+</script>
+
+<style scoped lang="scss">
+.sale {
+  :deep(.el-card__header) {
+    position: relative;
+    padding: 0 20px;
+    border-bottom: none;
+    .right {
+      position: absolute;
+      top: 0;
+      right: 20px;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      .el-date-editor {
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
--- src/views/home/monitor/index.vue
+++ src/views/home/monitor/index.vue
@@ -14,19 +14,21 @@
       <total-users></total-users>
     </el-col>
   </el-row>
+  <SaleForm></SaleForm>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 export default defineComponent({
-  name: 'Monitor'
+  name: "Monitor",
 });
 </script>
 <script lang="ts" setup>
-import TodayOrders from './components/TodayOrders.vue';
-import TodaySales from './components/TodaySales.vue';
-import TodayUsers from './components/TodayUsers.vue';
-import TotalUsers from './components/TotalUsers.vue';
+import TodayOrders from "./components/TodayOrders.vue";
+import TodaySales from "./components/TodaySales.vue";
+import TodayUsers from "./components/TodayUsers.vue";
+import TotalUsers from "./components/TotalUsers.vue";
+import SaleForm from "./components/SaleForm.vue";
 </script>
 
 <style lang="scss">
@@ -39,15 +41,15 @@ import TotalUsers from './components/TotalUsers.vue';
   .increment {
     width: 0;
     height: 0;
-    border-width:4px;
-    border-color:transparent  transparent green transparent;
+    border-width: 4px;
+    border-color: transparent transparent green transparent;
     border-style: solid;
-  } 
+  }
   .decrement {
     width: 0;
     height: 0;
-    border-width:4px;
-    border-color:red  transparent transparent transparent;
+    border-width: 4px;
+    border-color: red transparent transparent transparent;
     border-style: solid;
   }
 }
